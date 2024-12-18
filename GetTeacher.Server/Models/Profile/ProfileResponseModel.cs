33d9919138d98a7e8113336eb522b5fa665a3b2e
unknown <mechina-hanich-25@greendreamteam.onmicrosoft.com>
2024-12-18 19:45:50 +0200

    Return isTeacher and isStudent in profile

--- GetTeacher.Server/Models/Profile/ProfileResponseModel.cs
+++ GetTeacher.Server/Models/Profile/ProfileResponseModel.cs
@@ -1,8 +1,14 @@
-﻿namespace GetTeacherServer.Models.Profile;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetTeacherServer.Models.Profile;
 
 public class ProfileResponseModel
 {
     public string Result { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
+
+    public bool IsTeacher { get; set; } = false;
+
+    public bool IsStudent { get; set; } = false;
 }
