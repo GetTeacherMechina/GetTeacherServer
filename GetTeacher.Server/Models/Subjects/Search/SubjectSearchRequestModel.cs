272a8b48bbdd7202b6d0cb1d5796b08fb472aa05
aryeh15 <mechina-hanich-15@greendreamteam.onmicrosoft.com>
2024-12-25 20:24:15 +0200

    stupid name

--- GetTeacher.Server/Models/Subjects/Search/SubjectSearchRequestModel.cs
+++ GetTeacher.Server/Models/Subjects/Search/SubjectSearchRequestModel.cs
@@ -2,9 +2,5 @@
 
 public class SubjectSearchRequestModel
 {
-<<<<<<< HEAD
     public string subjectName { get; set; } = string.Empty;
-=======
-    public string SubjectName { get; set; } = string.Empty;
->>>>>>> 75765ba (added search subject controller)
 }
