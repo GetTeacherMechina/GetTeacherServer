75765ba95ba00e34bfa35dfd15b62514d710af3e
aryeh15 <mechina-hanich-15@greendreamteam.onmicrosoft.com>
2024-12-25 19:56:07 +0200

    added search subject controller

---
+++ GetTeacher.Server/Models/Subjects/Search/SubjectSearchResponseModel.cs
@@ -0,0 +1,8 @@
+﻿using GetTeacher.Server.Services.Database.Models;
+
+namespace GetTeacher.Server.Models.Subjects.Search;
+
+public class SubjectSearchResponseModel
+{
+    public ICollection<DbGrade> Subjects { get; set; } = new List<DbGrade>();
+}
