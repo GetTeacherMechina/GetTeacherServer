4c4b476e43c878746ef47985b61979faa64e84cc
yoav kaplan <mechina-hanich-24@greendreamteam.onmicrosoft.com>
2024-12-26 20:04:35 +0200

    added teacher subject controller

---
+++ GetTeacher.Server/Controllers/Teacher/TeacherSubjectSelectorController.cs
@@ -0,0 +1,55 @@
+﻿using GetTeacher.Server.Models.Teacher;
+using GetTeacher.Server.Services.Database.Models;
+using GetTeacher.Server.Services.Managers.Interfaces.UserManager;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace GetTeacher.Server.Controllers.Teacher
+{
+	[ApiController]
+	[Route("api/v1/TeacherSubjectSelectorController")]
+	public class TeacherSubjectSelectorController : ControllerBase
+	{
+		private readonly ITeacherManager teacherManager;
+		private readonly UserManager<DbUser> userManager;
+
+		public TeacherSubjectSelectorController(ITeacherManager teacherManager, UserManager<DbUser> userManager)
+		{
+			this.teacherManager = teacherManager;
+			this.userManager = userManager;
+		}
+
+		[HttpGet]
+		[Authorize]
+		public async Task<IActionResult> GetTeacherSubjects()
+		{
+			var emailClaim = User.FindFirst(ClaimTypes.Email);
+			if (emailClaim is null)
+				return BadRequest();
+
+			string email = emailClaim.Value;
+			DbUser? userResult = await userManager.FindByEmailAsync(email);
+			if (userResult == null)
+			{
+				Console.WriteLine("------------------------------hi------------------------------------------------");
+				return BadRequest(new TeacherSubjectsResponsModel());
+			}
+			DbTeacherSubject[] teacherSubjects = (await teacherManager.GetFromUser(userResult)).TeacherSubjects.ToArray();
+			String[] subject = new string[teacherSubjects.Length];
+			String[] grades = new string[teacherSubjects.Length];
+			for (int i = 0; i < teacherSubjects.Length; i++)
+			{
+				subject[i] = teacherSubjects[i].Subject.Name;
+				grades[i] = teacherSubjects[i].Grade.Name;
+			}
+			return Ok(new TeacherSubjectsResponsModel 
+			{
+				Grades = new String[] { "g", "g" },
+				Subjects = new String[]{ "g", "g" } 
+			});
+		}
+	}
+
+}
---
+++ GetTeacher.Server/Models/Teacher/TeacherSubjectsResponsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GetTeacher.Server.Models.Teacher;
+
+public class TeacherSubjectsResponsModel
+{
+	public string[]? Subjects { get; set; }
+
+	public string[]? Grades { get; set; }
+}
