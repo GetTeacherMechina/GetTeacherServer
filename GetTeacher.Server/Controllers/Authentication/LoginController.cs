705a36b83af4da72aedf45a2a6a6aea497ccb13e
Yonatan Ziv <yonatanmechina@gmail.com>
2025-01-06 19:46:48 +0200

    Handle a case where multiple instances of the same client try to connect by kicking the previous connection upon login

--- GetTeacher.Server/Controllers/Authentication/LoginController.cs
+++ GetTeacher.Server/Controllers/Authentication/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using GetTeacher.Server.Models.Authentication.Login;
 using GetTeacher.Server.Services.Database.Models;
 using GetTeacher.Server.Services.Managers.Interfaces;
+using GetTeacher.Server.Services.Managers.Interfaces.Networking;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,8 +9,9 @@ namespace GetTeacher.Server.Controllers.Authentication;
 
 [ApiController]
 [Route("api/v1/auth/[controller]")]
-public class LoginController(UserManager<DbUser> userManager, IJwtGenerator jwtTokenGenerator) : ControllerBase
+public class LoginController(IWebSocketSystem webSocketSystem, UserManager<DbUser> userManager, IUserStateTracker userStateTracker, IJwtGenerator jwtTokenGenerator) : ControllerBase
 {
+	private readonly IWebSocketSystem webSocketSystem = webSocketSystem;
 	private readonly UserManager<DbUser> userManager = userManager;
 	private readonly IJwtGenerator jwtTokenGenerator = jwtTokenGenerator;
 
@@ -19,7 +21,7 @@ public class LoginController(UserManager<DbUser> userManager, IJwtGenerator jwtT
 		DbUser? userResult = await userManager.FindByEmailAsync(loginModel.Email);
 		if (userResult == null)
 			return Unauthorized(new LoginResponseModel { Result = "Invalid email or password" });
-
+		
 		var passwordResult = await userManager.CheckPasswordAsync(userResult, loginModel.Password);
 
 		if (passwordResult)
@@ -27,6 +29,11 @@ public class LoginController(UserManager<DbUser> userManager, IJwtGenerator jwtT
 			string? jwt = jwtTokenGenerator.GenerateUserToken(userResult);
 			if (jwt is null)
 				return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred when generating a JWT.");
+
+			// TODO: Check if in a call BEFORE drop-kicking if online
+			// Drop-kick the previous client instance
+			if (userStateTracker.IsUserOnline(userResult))
+				webSocketSystem.RemoveWebSocket(userResult);
 
 			return Ok(new LoginResponseModel { Result = "Login successful", JwtToken = jwt });
 		}
