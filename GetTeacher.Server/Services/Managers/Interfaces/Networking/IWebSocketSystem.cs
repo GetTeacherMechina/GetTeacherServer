39f615255cce0164302f52020628d992c7195201
Yonatan Ziv <yonatanmechina@gmail.com>
2025-01-06 19:43:09 +0200

    Add receive functionality

--- GetTeacher.Server/Services/Managers/Interfaces/Networking/IWebSocketSystem.cs
+++ GetTeacher.Server/Services/Managers/Interfaces/Networking/IWebSocketSystem.cs
@@ -3,11 +3,15 @@ using GetTeacher.Server.Services.Database.Models;
 
 namespace GetTeacher.Server.Services.Managers.Interfaces.Networking;
 
+public record ReceiveResult(bool success, string message);
+
 public interface IWebSocketSystem
 {
 	public void AddWebSocket(DbUser user, WebSocket webSocket);
 
 	public void RemoveWebSocket(DbUser user);
+
+	public Task<ReceiveResult> ReceiveAsync(int clientId);
 
 	public Task<bool> SendAsync<T>(int clientId, T message);
 }
