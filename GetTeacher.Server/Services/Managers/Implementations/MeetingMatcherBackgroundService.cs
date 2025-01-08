c8ff97a8d9cdbc6d80d18e369a3c2fc7c7dc6bb2
Yonatan Ziv <yonatanmechina@gmail.com>
2025-01-09 00:13:00 +0200

    Hot fucking fix

--- GetTeacher.Server/Services/Managers/Implementations/MeetingMatcherBackgroundService.cs
+++ GetTeacher.Server/Services/Managers/Implementations/MeetingMatcherBackgroundService.cs
@@ -19,8 +19,10 @@ public class MeetingMatcherBackgroundService(IServiceProvider serviceProvider, I
 	private readonly ILogger<MeetingMatcherBackgroundService> logger = logger;
 	private readonly SemaphoreSlim trigger = new(0); // Semaphore for waking up the service
 
+	private readonly SemaphoreSlim matchSemaphore = new SemaphoreSlim(1, 1);
 	private readonly ConcurrentList<StudentEntry> studentsProcessingQueue = [];
 	private readonly ConcurrentList<StudentEntry> searchingStudents = [];
+	private readonly ConcurrentList<DbTeacher> csGoPhaseTeachers = [];
 
 	public void StartMatchStudent(DbStudent student, DbSubject subject)
 	{
@@ -68,14 +70,14 @@ public class MeetingMatcherBackgroundService(IServiceProvider serviceProvider, I
 		}
 	}
 
-	private async void StartAsyncMatches()
+	private void StartAsyncMatches()
 	{
 		while (studentsProcessingQueue.Count != 0)
 		{
 			StudentEntry studentEntry = studentsProcessingQueue.First();
 			studentsProcessingQueue.Remove(studentEntry);
 			// TODO: !!!!!
-			await ProcessStudentMatchAsync(studentEntry);
+			_ = ProcessStudentMatchAsync(studentEntry);
 		}
 	}
 
@@ -83,98 +85,116 @@ public class MeetingMatcherBackgroundService(IServiceProvider serviceProvider, I
 	{
 		searchingStudents.Add(studentEntry);
 
-		while (!studentEntry.StopMatchingCts.IsCancellationRequested)
-		{
-			ICollection<DbTeacher> teacherExclusions = [];
+		IServiceScope serviceScope = serviceProvider.CreateScope();
+		IWebSocketSystem webSocketSystem = serviceScope.ServiceProvider.GetRequiredService<IWebSocketSystem>();
+		ITeacherReadyManager teacherReadyManager = serviceScope.ServiceProvider.GetRequiredService<ITeacherReadyManager>();
 
-			IServiceScope serviceScope = serviceProvider.CreateScope();
-			IMeetingMatcher meetingMatcher = serviceScope.ServiceProvider.GetRequiredService<IMeetingMatcher>();
-			IWebSocketSystem webSocketSystem = serviceScope.ServiceProvider.GetRequiredService<IWebSocketSystem>();
-			IUserStateTracker userStateTracker = serviceScope.ServiceProvider.GetRequiredService<IUserStateTracker>();
-			ITeacherReadyManager teacherReadyManager = serviceScope.ServiceProvider.GetRequiredService<ITeacherReadyManager>();
+		// TODO: Plop the GUID from the database-backed session entry we create beforehand
+		string meetingGuid = Guid.NewGuid().ToString();
 
-			// TODO: Plop the GUID from the database-backed session entry we create beforehand
-			string meetingGuid = Guid.NewGuid().ToString();
-
-			bool studentOnline = true;
-			DbTeacher? foundTeacher = null;
-			while (foundTeacher is null)
-			{
-				if (!(studentOnline = userStateTracker.IsUserOnline(studentEntry.Student.DbUser)))
-					break;
-
-				foundTeacher = await meetingMatcher.MatchStudentTeacher(studentEntry.Student, studentEntry.Subject, teacherExclusions);
-
-				// TODO: Change this shit
-				if (foundTeacher is null)
-				{
-					try
-					{
-						await Task.Delay(500, studentEntry.StopMatchingCts.Token); // Nice
-					}
-					catch { }
-
-					continue;
-				}
-
-				CsGoContractRequestModel csGoContractRequestModel = new CsGoContractRequestModel
-				{
-					TeacherBio = foundTeacher.Bio,
-					TeacherRank = foundTeacher.Rank,
-					MeetingResponseModel = new MeetingResponseModel
-					{
-						MeetingGuid = meetingGuid,
-						CompanionName = foundTeacher.DbUser.UserName!,
-					}
-				};
-
-				await webSocketSystem.SendAsync(studentEntry.Student.DbUserId, csGoContractRequestModel);
-				ReceiveResult wsReadResult = await webSocketSystem.ReceiveAsync(studentEntry.Student.DbUserId);
-
-				if (!wsReadResult.Success)
-				{
-					// TODO: Handle
-					break;
-				}
-
-				string csgoConfirmOrDeny = wsReadResult.Message;
-				if (csgoConfirmOrDeny != csGoConfirm && csgoConfirmOrDeny != csGoDeny)
-				{
-					// TODO: Handle
-					break;
-				}
-
-				if (csgoConfirmOrDeny == csGoDeny)
-				{
-					teacherExclusions.Add(foundTeacher);
-					foundTeacher = null;
-				}
-			}
-
-			if (!studentOnline || foundTeacher is null)
-				continue;
-
-			teacherReadyManager.NotReadyToTeach(foundTeacher);
-			// Notify student and teacher :)
-			try
-			{
-				MeetingResponseModel meetingResponseModel = new MeetingResponseModel
-				{
-					MeetingGuid = meetingGuid,
-					CompanionName = studentEntry.Student.DbUser.UserName!
-				};
-
-				await webSocketSystem.SendAsync(foundTeacher.DbUserId, meetingResponseModel);
-			}
-			catch (Exception ex)
-			{
-				logger.LogError(ex, "An unexpected expection happened when trying to match student and teacher.");
-			}
-
-			// TODO: Remove this shit with the while condition
+		DbTeacher? teacher = await MatchTeacher(studentEntry, meetingGuid);
+		if (teacher is null)
 			return;
+
+		teacherReadyManager.NotReadyToTeach(teacher);
+
+		// Notify student and teacher :)
+		try
+		{
+			MeetingResponseModel meetingResponseModel = new MeetingResponseModel
+			{
+				MeetingGuid = meetingGuid,
+				CompanionName = studentEntry.Student.DbUser.UserName!
+			};
+
+			await webSocketSystem.SendAsync(teacher.DbUserId, meetingResponseModel);
+		}
+		catch (Exception ex)
+		{
+			logger.LogError(ex, "An unexpected expection happened when trying to match student and teacher.");
 		}
 
 		searchingStudents.Remove(studentEntry);
+	}
+
+	private async Task<DbTeacher?> MatchTeacher(StudentEntry studentEntry, string meetingGuid)
+	{
+		IServiceScope serviceScope = serviceProvider.CreateScope();
+		IMeetingMatcher meetingMatcher = serviceScope.ServiceProvider.GetRequiredService<IMeetingMatcher>();
+
+		ICollection<DbTeacher> teacherExclusions = [];
+		DbTeacher? foundTeacher = null;
+
+		while (foundTeacher is null && !studentEntry.StopMatchingCts.IsCancellationRequested)
+		{
+			await matchSemaphore.WaitAsync();
+			try
+			{
+				foundTeacher = await meetingMatcher.MatchStudentTeacher(studentEntry.Student, studentEntry.Subject, teacherExclusions.Concat(csGoPhaseTeachers).ToList());
+
+				if (foundTeacher is null)
+				{
+					await Task.Delay(500);
+					continue;
+				}
+
+				csGoPhaseTeachers.Add(foundTeacher);
+			}
+			finally
+			{
+				matchSemaphore.Release();
+			}
+
+
+			if (await CsGoContract(studentEntry, foundTeacher, meetingGuid))
+				break;
+			else
+			{
+				teacherExclusions.Add(foundTeacher);
+				csGoPhaseTeachers.Remove(foundTeacher);
+			}
+		}
+
+		if (studentEntry.StopMatchingCts.IsCancellationRequested)
+			return null;
+
+		csGoPhaseTeachers.Remove(foundTeacher!);
+		return foundTeacher;
+	}
+
+	private async Task<bool> CsGoContract(StudentEntry studentEntry, DbTeacher teacher, string meetingGuid)
+	{
+		IServiceScope serviceScope = serviceProvider.CreateScope();
+		IMeetingMatcher meetingMatcher = serviceScope.ServiceProvider.GetRequiredService<IMeetingMatcher>();
+		IWebSocketSystem webSocketSystem = serviceScope.ServiceProvider.GetRequiredService<IWebSocketSystem>();
+
+		CsGoContractRequestModel csGoContractRequestModel = new CsGoContractRequestModel
+		{
+			TeacherBio = teacher.Bio,
+			TeacherRank = teacher.Rank,
+			MeetingResponseModel = new MeetingResponseModel
+			{
+				MeetingGuid = meetingGuid,
+				CompanionName = teacher.DbUser.UserName!,
+			}
+		};
+
+		await webSocketSystem.SendAsync(studentEntry.Student.DbUserId, csGoContractRequestModel);
+		ReceiveResult wsReadResult = await webSocketSystem.ReceiveAsync(studentEntry.Student.DbUserId);
+
+		if (!wsReadResult.Success)
+			return false;
+
+		string csgoConfirmOrDeny = wsReadResult.Message;
+		if (csgoConfirmOrDeny != csGoConfirm && csgoConfirmOrDeny != csGoDeny)
+			return false;
+
+		if (csgoConfirmOrDeny == csGoDeny)
+			return false;
+
+		if (csgoConfirmOrDeny == csGoConfirm)
+			return true;
+
+		return false;
 	}
 }
