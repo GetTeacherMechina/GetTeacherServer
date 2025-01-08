d119f0def5f508ea517443ee4be7893147094b49
Yonatan Ziv <yonatanmechina@gmail.com>
2025-01-08 19:33:18 +0200

    Email sender working

---
+++ GetTeacher.Server/Services/Managers/Implementations/EmailSender/IdentityEmailSender.cs
@@ -0,0 +1,22 @@
+﻿using GetTeacher.Server.Services.Database.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace GetTeacher.Server.Services.Managers.Implementations.EmailSender;
+
+public class IdentityEmailSender : IEmailSender<DbUser>
+{
+	public Task SendConfirmationLinkAsync(DbUser user, string email, string confirmationLink)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task SendPasswordResetCodeAsync(DbUser user, string email, string resetCode)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task SendPasswordResetLinkAsync(DbUser user, string email, string resetLink)
+	{
+		throw new NotImplementedException();
+	}
+}
