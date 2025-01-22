69f3d457d16bf24f1a867b54cce571d7e089d623
Yonatan Ziv <yonatanmechina@gmail.com>
2025-01-23 01:29:59 +0200

    Polish

--- GetTeacher.Server/Controllers/reportTeacher/ReportTeacherController.cs
+++ GetTeacher.Server/Controllers/reportTeacher/ReportTeacherController.cs
@@ -1,32 +1,35 @@
 ﻿using GetTeacher.Server.Models.ReportTeacher;
+using GetTeacher.Server.Services.Database;
 using GetTeacher.Server.Services.Database.Models;
 using GetTeacher.Server.Services.Managers.Interfaces;
 using GetTeacher.Server.Services.Managers.Interfaces.UserManager;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Runtime.CompilerServices;
 
 namespace GetTeacher.Server.Controllers.reportTeacher;
 
 [ApiController]
-[Route("/api/v1/Report-teacher-controller")]
-public class ReportTeacherController(
-	IMeetingManager meetingManager, IStudentManager studentManager) : ControllerBase
+[Route("/api/v1/report-teacher-controller")]
+public class ReportTeacherController(GetTeacherDbContext getTeacherDbContext, IMeetingManager meetingManager, IStudentManager studentManager) : ControllerBase
 {
+	private readonly GetTeacherDbContext getTeacherDbContext = getTeacherDbContext;
 	private readonly IMeetingManager meetingManager = meetingManager;
 	private readonly IStudentManager studentManager = studentManager;
 
 	[HttpPost]
-	
-	public async Task<IActionResult> ReportLastTeacher([FromBody] ReportTeacherModel request) 
+	[Authorize]
+	public async Task<IActionResult> ReportTeacher([FromBody] ReportTeacherRequestModel request) 
 	{
 		DbStudent? student = await studentManager.GetFromUser(User);
 		if (student is null)
-		{
-			return BadRequest("student not found");
-		}
-		ICollection<DbMeeting> meetings = await meetingManager.GetAllStudentMeetings(student);
-		DbTeacher teacher = meetings.OrderByDescending(a => a.EndTime).ToArray()[0].Teacher;
-		teacher.Reports.Add(request.report);
+			return BadRequest("Student not found");
+
+		DbMeeting? meeting = await meetingManager.GetMeeting(request.MeetingGuid);
+		if (meeting is null)
+			return BadRequest("Meeting not found");
+
+		meeting.Teacher.Reports.Add(request.ReportContent);
+		await getTeacherDbContext.SaveChangesAsync();
 		return Ok(new { });
 	}
 }
